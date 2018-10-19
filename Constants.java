e2c9dded7e722ef6e4bb603e6ee3e3f432d834e2
scourgesong <44284708+scourgesong@users.noreply.github.com>
2018-10-19 16:36:44 +0300

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,9 @@
+package labirint;
+
+public class Constants {
+	public static final String WALL = "w";
+	public static final String TERRAIN = "t";
+	public static final String SNARE = "s";
+	public static final String BONUS = "b";
+	public static final String LIFE = "l";
+}
